c4af32539d86c2526097a2440db7884b04b5515f
szczep4niak <jjdroid6969@gmail.com>
2020-10-22 22:26:17 +0200

    login query string to post with body

--- src/app.ts
+++ src/app.ts
@@ -1,77 +1,77 @@
-import express from "express"
-import { notFound, errorHandler } from "./middlewares"
+import express from 'express';
+import { notFound, errorHandler } from './middlewares';
 
-import path from "path"
+import path from 'path';
 
-const volleyball = require("volleyball")
-const cors = require("cors")
-const morgan = require("morgan")
-const helmet = require("helmet")
+const volleyball = require('volleyball');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
-const repo = require("./routes/repo")
-const file = require("./routes/file")
-const session = require("express-session")
+const repo = require('./routes/repo');
+const file = require('./routes/file');
+const session = require('express-session');
 
-const app = express()
+const app = express();
 
-app.use(session({ secret: "keyboardCat" }))
-app.use(express.static(path.join(__dirname, "public")))
+app.use(session({ secret: 'keyboardCat' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan("dev"))
-app.use(volleyball)
-app.use(helmet())
+app.use(morgan('dev'));
+app.use(volleyball);
+app.use(helmet());
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // http://localhost:PORT
-app.get("/", (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.json({
-    message: "=> from /",
-  })
-})
+    message: '=> from /',
+  });
+});
 
 // http://localhost:PORT/login?queryString
-app.get("/login", (req, res, next) => {
-  const { accessToken, username } = req.query
+app.post('/login', (req, res, next) => {
+  const { accessToken, username } = req.body;
 
   const user = {
     accessToken: accessToken?.toString(),
     username: username?.toString(),
-  }
+  };
 
   if (!user.accessToken || !user.username) {
-    const error = new Error("user not found")
-    return next(error)
+    const error = new Error('user not found');
+    return next(error);
   }
 
-  req.session.user = user
+  req.session.user = user;
 
-  res.redirect("/home")
-})
+  res.redirect('/home');
+});
 
-app.get("/home", (req, res, next) => {
+app.get('/home', (req, res, next) => {
   // TODO: check if user is authenticated
   if (req.session.user) {
-    res.sendFile(path.join(__dirname + "/public/home.html"))
+    res.sendFile(path.join(__dirname + '/public/home.html'));
   } else {
-    const error = new Error("user not granted")
-    next(error)
+    const error = new Error('user not granted');
+    next(error);
   }
-})
+});
 
 // /API ENDPOINT ROUTES
 
 // localhost:3000/api/=>
-app.use("/api", repo)
-app.use("/file", file)
+app.use('/api', repo);
+app.use('/file', file);
 
 // ERROR HANDLERS
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`repo service is listening at port ${PORT}!`)
-})
+  console.log(`repo service is listening at port ${PORT}!`);
+});
--- src/routes/repo/index.ts
+++ src/routes/repo/index.ts
@@ -1,166 +1,154 @@
-import { Router, Request, Response, NextFunction } from "express"
-import { Octokit } from "@octokit/core"
-import { exec } from "child_process"
+import { Router, Request, Response, NextFunction } from 'express';
+import { Octokit } from '@octokit/core';
+import { exec } from 'child_process';
 
-require("dotenv").config()
+require('dotenv').config();
 
-const repo = Router()
-
-// MOCKED USER DATA
-const userData = {
-  githubId: process.env.USER_ID,
-  accessToken: process.env.USER_ACCESS_TOKEN,
-  username: process.env.USER_NAME,
-  displayName: "null",
-  repoName: "notes",
-}
+const repo = Router();
 
 // http://localhost:PORT/api
-repo.get("/", (req: Request, res: Response, next: NextFunction): void => {
-  // const { githubId, accessToken, username, displayName } = req.query
-  // const userData: Request["user"] = {
-  //   githubId: githubId as string,
-  //   accessToken: accessToken as string,
-  //   username: username as string,
-  //   displayName: displayName as string,
-  // }
-
-  const { githubId, accessToken } = userData
-
-  if (!githubId || !accessToken) {
-    const error = new Error("user not found")
-    res.status(404)
-    return next(error)
-  }
-
+repo.get('/', (req: Request, res: Response, next: NextFunction): void => {
   res.json({
-    message: "=> from /api",
-    user: req.session.user
-  })
-})
+    message: '=> from /api',
+  });
+});
 
 // http://localhost:PORT/api/find-repo/:name
-repo.get("/find-repo/:name", async (req: Request, res: Response, next: NextFunction) => {
-  const { name: repoName } = req.params
-  const { username, accessToken } = req.session.user;
+repo.get(
+  '/find-repo/:name',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name: repoName } = req.params;
+    const { username, accessToken } = req.session.user;
 
-  const fullRepoName = `${username}/${repoName}`
+    const fullRepoName = `${username}/${repoName}`;
 
-  const octokit = new Octokit({ auth: accessToken })
+    const octokit = new Octokit({ auth: accessToken });
 
-  try {
-    const {
-      data: { html_url },
-    } = await octokit.request(`GET /repos/${fullRepoName}`)
-    res.json({
-      url: html_url,
-    })
-  } catch (error) {
-    return next(error)
+    try {
+      const {
+        data: { html_url },
+      } = await octokit.request(`GET /repos/${fullRepoName}`);
+      res.json({
+        url: html_url,
+      });
+    } catch (error) {
+      return next(error);
+    }
   }
-})
+);
 
-repo.post("/create-repo/:name", async (req: Request, res: Response, next: NextFunction) => {
-  const { name: repoName } = req.params
-  const { accessToken } = req.session.user;
+repo.post(
+  '/create-repo/:name',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name: repoName } = req.params;
+    const { accessToken } = req.session.user;
 
-  const octokit = new Octokit({ auth: accessToken })
+    const octokit = new Octokit({ auth: accessToken });
 
-  try {
-    await octokit.request("POST /user/repos", {
-      name: repoName,
-    })
+    try {
+      await octokit.request('POST /user/repos', {
+        name: repoName,
+      });
 
-    res.json({
-      info: "repo has been created",
-    })
-  } catch (error) {
-    return next(error)
+      res.json({
+        info: 'repo has been created',
+      });
+    } catch (error) {
+      return next(error);
+    }
   }
-})
+);
 
-repo.post("/create-repo/:name", async (req: Request, res: Response, next: NextFunction) => {
-  const { name: repoName } = req.params
-  const { accessToken } = req.session.user;
+repo.post(
+  '/create-repo/:name',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name: repoName } = req.params;
+    const { accessToken } = req.session.user;
 
-  const octokit = new Octokit({ auth: accessToken })
+    const octokit = new Octokit({ auth: accessToken });
 
-  try {
-    await octokit.request("POST /user/repos", {
-      name: repoName,
-    })
+    try {
+      await octokit.request('POST /user/repos', {
+        name: repoName,
+      });
 
-    res.json({
-      info: "repo has been created",
-    })
-  } catch (error) {
-    return next(error)
+      res.json({
+        info: 'repo has been created',
+      });
+    } catch (error) {
+      return next(error);
+    }
   }
-})
+);
 
-repo.post("/save/:name", async (req: Request, res: Response, next: NextFunction) => {
-  const { name: repoName } = req.params
+repo.post(
+  '/save/:name',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name: repoName } = req.params;
+
+    const message = 'commit';
+
+    // relative path issue
+    const cdToDockerVolume = 'cd ../repository';
+
+    const gitAdd = 'git add .';
+    const gitCommit = `git commit -m "${message}"`;
+    const gitPush = `git push`;
+
+    exec(
+      ` ${cdToDockerVolume}; ${gitAdd}; ${gitCommit}; ${gitPush}`,
+      (err, stdout, stderr) => {
+        // TODO: done handler flag
+
+        if (err) {
+          console.error('push error', repoName);
+          console.error(err);
+          next(err);
+        } else {
+          console.log('push done', repoName);
+          console.log(stdout);
+          res.json({
+            message: stdout || 'changes has been saved',
+            // done | save
+          });
+        }
+      }
+    );
+  }
+);
+
+repo.get('/update', async (req: Request, res: Response, next: NextFunction) => {
+  const { name: repoName } = req.params;
   const { accessToken, username } = req.session.user;
 
-  const message = "commit"
-
   // relative path issue
-  const cdToDockerVolume = "cd ../repository"
-
-  const gitAdd = "git add ."
-  const gitCommit = `git commit -m "${message}"`
-  const gitPush = `git push`
-
-  exec(` ${cdToDockerVolume}; ${gitAdd}; ${gitCommit}; ${gitPush}`, (err, stdout, stderr) => {
-    // TODO: done handler flag
-
-    if (err) {
-      console.error("push error", repoName)
-      console.error(err)
-      next(err)
-    } else {
-      console.log("push done", repoName)
-      console.log(stdout)
-      res.json({
-        message: stdout || "changes has been saved",
-        // done | save
-      })
-    }
-  })
-})
-
-repo.get("/update", async (req: Request, res: Response, next: NextFunction) => {
-  const { name: repoName } = req.params
-  const { accessToken, username} = req.session.user;
-
-  // relative path issue
-  const cdToDockerVolume = "cd ../repository"
-  const doesRepoExist = "[ -d .git ];"
-  const gitFetch = "git fetch --all; git reset --hard origin/master; "
-  const gitClone = `git clone https://${accessToken}@github.com/${username}/${repoName}.git .;`
-  const setInitialConfig = `git config user.name "${username}"; git config user.email "<>";`
+  const cdToDockerVolume = 'cd ../repository';
+  const doesRepoExist = '[ -d .git ];';
+  const gitFetch = 'git fetch --all; git reset --hard origin/master; ';
+  const gitClone = `git clone https://${accessToken}@github.com/${username}/${repoName}.git .;`;
+  const setInitialConfig = `git config user.name "${username}"; git config user.email "<>";`;
 
   exec(
     ` ${cdToDockerVolume}; if ${doesRepoExist} then ${gitFetch} else ${gitClone} ${setInitialConfig} fi;  `,
     (err, stdout, stderr) => {
       // TODO: done handler flag
       if (err) {
-        console.error("cloning error", repoName)
-        console.error(err)
-        next(err)
+        console.error('cloning error', repoName);
+        console.error(err);
+        next(err);
       } else {
-        console.log("cloning done", repoName)
-        console.log(stdout)
+        console.log('cloning done', repoName);
+        console.log(stdout);
         res.json({
-          message: stdout || "repo has been cloned",
+          message: stdout || 'repo has been cloned',
           // done | save
-        })
+        });
       }
     }
-  )
-})
+  );
+});
 
-module.exports = repo
+module.exports = repo;
 
 // POBRANIE STR DANYCH Z REPO
 // ZAŁADOWANIE POJEDYŃCZEGO PLIKU Z REPO md.FILE to txtarea
